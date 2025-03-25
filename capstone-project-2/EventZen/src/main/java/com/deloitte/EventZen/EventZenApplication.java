9a77a76ac75a85d43bb3b750abecab96620bee19
Atharva <dummy@gmail.com>
2025-03-25 10:54:48 +0530

    commit-initial-setup

---
+++ capstone-project-2/EventZen/src/main/java/com/deloitte/EventZen/EventZenApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.EventZen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventZenApplication.class, args);
+	}
+
+}
